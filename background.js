1aeb47d5a9900c136e7b681b31ace897e729f84b
kaR07L6 <u.rinu.mao0624@icloud.com>
2025-12-08 12:59:01 +0900

    aa

--- background.js
+++ background.js
@@ -1,18 +1,16 @@
-// 右クリックメニューの作成
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.contextMenus.create({
-      id: "copyCitation",
-      title: "引用形式でコピー",
-      contexts: ["selection"]
+  chrome.contextMenus.create({
+    id: "copyCitation",
+    title: "引用形式でコピー",
+    contexts: ["selection"]
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "copyCitation") {
+    chrome.tabs.sendMessage(tab.id, {
+      action: "getCitation",
+      selectedText: info.selectionText
     });
-  });
-  
-  // メニュークリック時の処理
-  chrome.contextMenus.onClicked.addListener((info, tab) => {
-    if (info.menuItemId === "copyCitation") {
-      chrome.tabs.sendMessage(tab.id, {
-        action: "getCitation",
-        selectedText: info.selectionText
-      });
-    }
-  });
+  }
+});
